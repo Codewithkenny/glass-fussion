23d755a45d8d7b2a2e31e6dc8de324934f62472d
Olapoju Kenny Agbomeji <103900061+Codewithkenny@users.noreply.github.com>
2022-11-12 07:59:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const myMenu2 = document.querySelector('closebtn');
+let hamburger = false;
+myMenu2.addEventListener('click', ()=>{
+    if (!hamburger){
+        myMenu2.classList.add('open')
+    }
+})
+const myMenu = document.querySelector('hamburger').style.width = "250px"
+
+
+function myMenu2 (){
+const myMenu2 = document.querySelector('closebtn').style.marginLeft = "250px";
+};
